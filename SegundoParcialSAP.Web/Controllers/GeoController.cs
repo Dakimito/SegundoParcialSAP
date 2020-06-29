72ee3acae8f9a79735f3d2d2272ce15b7430f653
Damian Contreras <damiancontreras.15@gmail.com>
2020-06-29 02:41:50 -0300

    Agregar archivos de proyecto.

---
+++ SegundoParcialSAP.Web/Controllers/GeoController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SegundoParcialSAP.Web.Controllers
+{
+    public class GeoController : Controller
+    {
+        // GET: Geo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Geo/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Geo/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Geo/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Geo/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Geo/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Geo/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Geo/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SegundoParcialSAP.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SegundoParcialSAP.Web.MvcApplication" Language="C#" %>
